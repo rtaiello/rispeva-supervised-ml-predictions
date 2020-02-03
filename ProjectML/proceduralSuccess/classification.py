3a7baf0bfa3d9ed7028b8c9e12130c013fd95f9d
Bipolski <beatricepolacchi@gmail.com>
2020-02-03 09:52:22 +0100

    selected features = 30. svm of degree 3.

--- ProjectML/proceduralSuccess/classification.py
+++ ProjectML/proceduralSuccess/classification.py
@@ -19,13 +19,13 @@ def decision_tree_classifier(X_train, y_train,classWeights):
     return clf
 
 def ensemble_bagging(X_train, y_train):
-    #classifier = DecisionTreeClassifier(random_state=SEED,n_jobs=-1, class_weight='balanced',max_depth=1)
-    clf = BaggingClassifier(base_estimator=None, n_estimators=500, n_jobs=-1)
+    classifier = svm.SVC(C=1.0, kernel='poly', degree = 2, random_state=SEED)
+    clf = BaggingClassifier(base_estimator=classifier, n_estimators=300, n_jobs=-1)
     clf.fit(X_train, y_train)
     return clf
 
 def ensemble_random_forest(X_train, y_train):
-    clf = RandomForestClassifier(n_estimators=200, criterion='gini',max_features=15, n_jobs=-1,class_weight='balanced',
+    clf = RandomForestClassifier(n_estimators=300, criterion='gini',max_features=10, n_jobs=-1,class_weight='balanced',
                                random_state=SEED)
     # Fit 'rf' to the training set
     clf.fit(X_train, y_train)
@@ -38,9 +38,9 @@ def ensemble_ada_boosting(X_train, y_train):
     clf.fit(X_train, y_train)
     return clf
 
-def svm_classifier(X_train,y_train, class_dict):
-    x_scaled = StandardScaler().fit_transform(X_train)
-    clf = svm.SVC(C=1.0, kernel='linear',degree=3, class_weight=class_dict, max_iter=-1,  random_state=SEED).fit(X_train, y_train)
+def svm_classifier(X_train,y_train):
+    #x_scaled = StandardScaler().fit_transform(X_train)
+    clf = svm.SVC(C=1.0, kernel='poly', degree=3, class_weight='balanced', max_iter=-1,  random_state=SEED).fit(X_train, y_train)
     return clf
 
 def ensemble_voting(X_train,y_train):
--- ProjectML/proceduralSuccess/main.py
+++ ProjectML/proceduralSuccess/main.py
@@ -22,24 +22,29 @@ dt = my_l_extract_feature(DT_KNN, LABEL)  #3570
 X = dt.loc[:, 'CenterID':'P2Y12 inhibt']
 y = dt.loc[:, LABEL]
 
+
+
 #feature selection with Pearson correlation
-# cor_support, cor_feature = cor_selector(X, y, 40)
-# print(str(len(cor_feature)), 'selected features')
-# print(cor_feature)
-# X = dt.loc[:, cor_feature]
+cor_support, cor_feature = cor_selector(X, y, 30)
+print(str(len(cor_feature)), 'selected features')
+print(cor_feature)
+X = dt.loc[:, cor_feature]
 
 #feature selection with RFE
-clf = clf = RandomForestClassifier(n_estimators=200, criterion='gini', max_features=20, n_jobs=-1,class_weight='balanced', random_state=SEED)
-rfe_selector = RFE(estimator=clf, n_features_to_select=40, step=10, verbose=5)
-rfe_selector.fit(X, y)
-rfe_support = rfe_selector.get_support()
-rfe_feature = X.loc[:,rfe_support].columns.tolist()
-print(str(len(rfe_feature)), 'selected features')
+# classifier = svm.SVC(kernel='poly', degree=2, random_state=SEED)
+# clf = AdaBoostClassifier(base_estimator=classifier, n_estimators=100, algorithm=SAMME)
+# rfe_selector = RFE(estimator=clf, n_features_to_select=20, step=10, verbose=5)
+# rfe_selector.fit(x_scaled, y)
+# rfe_support = rfe_selector.get_support()
+# rfe_feature = X.loc[:,rfe_support].columns.tolist()
+# print(str(len(rfe_feature)), 'selected features')
+
+x_scaled = StandardScaler().fit_transform(X)
 
 class_1 = dt[dt.ProceduralSuccess == True].count()  #3085
 class_0 = dt[dt.ProceduralSuccess == False].count()  #422
 
-X_train_val, X_test, y_train_val, y_test = my_l_split(X, y, testSize=0.1)
+X_train_val, X_test, y_train_val, y_test = my_l_split(x_scaled, y, testSize=0.1)
 class_1_test = y_test[y_test == True].count()
 class_0_test = y_test[y_test == False].count()
 
@@ -49,7 +54,9 @@ class_0_val = y_val[y_val == False].count()
 
 #clf = ensemble_random_forest(X_train, y_train)   #best
 #clf = ensemble_ada_boosting(X_train,y_train)    #worst
-clf = ensemble_bagging(X_train,y_train)      #medium
+#clf = ensemble_bagging(X_train,y_train)      #medium
+
+clf = svm_classifier(X_train,y_train)
 
 y_pred_stack = clf.predict(X_val)
 y_pred = clf.predict(X_test)
