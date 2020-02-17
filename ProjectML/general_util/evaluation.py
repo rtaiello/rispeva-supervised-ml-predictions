d67385ad1f6f6b947b3ccbba5c3a3c97cfceba44
Bipolski <beatricepolacchi@gmail.com>
2020-02-17 19:08:27 +0100

    added my_cross_f1 function by Bea

--- ProjectML/general_util/evaluation.py
+++ ProjectML/general_util/evaluation.py
@@ -2,6 +2,11 @@
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
+from sklearn.model_selection import StratifiedKFold
+import pandas as pd
+from sklearn.metrics import f1_score
+import numpy as np
+SEED=1
 
 
 def my_l_confusion_matrix(y_test, y_pred):
@@ -27,3 +32,25 @@ def get_balanced_accuracy(X, y, clf):
 def report(y_true, y_pred):
     class_report = classification_report(y_true, y_pred)
     return class_report
+
+def my_cross_f1(X,y,clf,cv) :
+    skf = StratifiedKFold(n_splits=cv, random_state=SEED, shuffle=True)
+    f1_list_0 = []
+    f1_list_1 = []
+    train_list = []
+    test_list = []
+    for train_index, test_index in skf.split(X, y):
+        train_list = pd.concat([X.iloc[train_index.tolist(), :], y.iloc[train_index.tolist()]], axis=1)
+        test_list = pd.concat([X.iloc[test_index.tolist(), :], y.iloc[test_index.tolist()]], axis=1)
+        clf.fit(X.iloc[train_index.tolist(), :], y.iloc[train_index.tolist()])
+        y_predicted = clf.predict(X.iloc[test_index.tolist(), :])
+        f1_list_0.append(f1_score(abs(y.iloc[test_index.tolist()] - 1), abs(y_predicted - 1)))
+        f1_list_1.append(f1_score(y.iloc[test_index.tolist()], y_predicted))
+    f1_list_0 = np.array(f1_list_0)
+    f1_list_1 = np.array(f1_list_1)
+    f1_mean0 = f1_list_0.mean()
+    f1_std0 = f1_list_0.std()
+    f1_mean1 = f1_list_1.mean()
+    f1_std1 = f1_list_1.std()
+    return f1_mean0, f1_std0, f1_mean1, f1_std1
+
--- ProjectML/proceduralSuccess/main.py
+++ ProjectML/proceduralSuccess/main.py
@@ -8,10 +8,8 @@ from ProjectML.proceduralSuccess.feature_extraction import *
 from ProjectML.general_util.pre_processing import *
 from ProjectML.general_util.evaluation import *
 # third part
-from sklearn.metrics import f1_score
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import StratifiedKFold
 import pandas as pd
 # constant
 from ProjectML.general_util.constant import *
@@ -110,20 +108,9 @@ print(rep_test2)
 # print('Report of ROC score on val set')
 # print(score)
 
-clf = svm.SVC(C=100.0, kernel='rbf', class_weight='balanced', max_iter=-1, random_state=SEED).fit(X_train, y_train)
-skf = StratifiedKFold(n_splits=10, random_state=SEED, shuffle=True)
-f1_list = []
-train_list = []
-test_list = []
-for train_index, test_index in skf.split(X, y):
-    train_list = pd.concat([X.iloc[train_index.tolist(), :], y.iloc[train_index.tolist()]], axis=1)
-    test_list = pd.concat([X.iloc[test_index.tolist(), :], y.iloc[test_index.tolist()]], axis=1)
-    clf.fit(X.iloc[train_index.tolist(), :], y.iloc[train_index.tolist()])
-    y_predicted = clf.predict(X.iloc[test_index.tolist(), :])
-    f1_list.append(f1_score(abs(y.iloc[test_index.tolist()] - 1), abs(y_predicted - 1)))
-f1_list = np.array(f1_list)
-print(f1_list)
-print("F1 score (0 class): %0.2f (+/- %0.2f)" % (f1_list.mean(), f1_list.std() * 1.95))
+clf = RandomForestClassifier(n_estimators=500, criterion='gini', max_features=5, n_jobs=-1, class_weight='balanced',random_state=SEED)
+f1_mean0, f1_std0, f1_mean1, f1_std1 = my_cross_f1(X,y,clf,cv=10)
+print("F1 score (0 class): %0.2f (+/- %0.2f)" % (f1_mean0, f1_std0 * 1.95))
 
 # PLOT PAIRPLOT
 
